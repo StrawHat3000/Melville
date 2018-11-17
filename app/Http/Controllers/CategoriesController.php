40b67ca379dd5b9a12c839771ddfa2f4c16324f3
edun-abayomi <edunhenry@gmail.com>
2018-11-17 14:28:24 +0000

    Updated CategoriesController

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -107,6 +107,12 @@ class CategoriesController extends Controller
     {
         $category = Category::find($id);
 
+        foreach($category->posts as $post){
+
+          $post->forceDelete();
+
+        }
+
         $category->delete();
 
         Session::flash('success', 'You successfully deleted the category');
