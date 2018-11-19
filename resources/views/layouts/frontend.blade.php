0e2bb490fc46be4f88a438cbefadb09b3897d12e
edun-abayomi <edunhenry@gmail.com>
2018-11-19 16:38:04 +0000

    Updated Homepage, included header and footer files, created frontend template

---
+++ resources/views/layouts/frontend.blade.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+
+<head>
+    <!-- Meta -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <meta name="author" content="">
+    <!-- Page Title -->
+    <title>MelVille Group Of Schools</title>
+    <!-- Place favicon.ico in the root directory -->
+    <link rel="apple-touch-icon" href="apple-touch-icon.html">
+    <link rel="shortcut icon" href="favicon.ico">
+    <!-- Google Fonts css-->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Roboto+Slab:300,400,700" rel="stylesheet">
+
+    <!-- Bootstrap css -->
+    <link href="{{ asset('assets/css/bootstrap.min.css') }}" rel="stylesheet" media="screen">
+    <!-- Icofont css-->
+    <link href="{{ asset('assets/icofont/icofont.min.css') }}" rel="stylesheet" media="screen">
+    <!-- FlatIcon css-->
+    <link href="{{ asset('assets/font/flaticon.css') }}" rel="stylesheet" media="screen">
+    <!-- Carousel css -->
+    <link href="{{  asset('assets/css/owl.carousel.min.css') }}" rel="stylesheet" media="screen">
+    <!-- Carousel default css -->
+    <link href=" {{ asset('assets/css/owl.theme.default.min.css')  }}" rel="stylesheet" media="screen">
+    <!-- Animate css -->
+    <link href="{{ asset('assets/css/animate.css') }}" rel="stylesheet" media="screen">
+    <!-- Lity css -->
+    <link href=" {{ asset('assets/css/lity.css') }}" rel="stylesheet" media="screen">
+    <!-- Style css -->
+    <link href="{{ asset('assets/css/style.css') }}" rel="stylesheet" media="screen">
+    <!-- Responsive css -->
+    <link rel="stylesheet" href="{{ asset('assets/css/responsive.css') }}">
+    <!--Modernizr Js-->
+    <script src="{{ asset('assets/js/vendor/modernizr-3.6.0.min.js')  }}"></script>
+
+    </head>
+  <!--[if lte IE 9]>
+    <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
+  <![endif]-->
+
+<body>
+
+
+    @include('includes.header')
+
+
+
+    <!-- About Us -->
+
+
+   <!--  What We Offer -->
+
+   <!--  What We Offer -->
+
+  <!-- Home Contact Form -->
+
+  <!-- Home Contact Form -->
+
+
+   <!-- What people says Area -->
+
+   <!-- What people says Area -->
+
+    <!-- For Mobile and Small Devices Section -->
+
+    <!-- For Mobile and Small Devices Section -->
+
+
+
+   <!--  Latest Blog News -->
+
+   <!--  Latest Blog News -->
+
+
+   <!--  Looking For Contsultant -->
+
+   <!--  Looking For Contsultant -->
+
+
+   <!-- Footer Top Area -->
+    @include('includes.footer')
+    <!-- Back to scroll  -->
+
+    <!--All Jquery Library Files -->
+    <script src="{{  asset('assets/js/vendor/jquery-3.3.1.min.js') }}"></script>
+    <!-- Carousel js  -->
+    <script src="{{ asset('assets/js/owl.carousel.min.js') }}"></script>
+    <!-- Bootstrap js  -->
+    <script src="{{ asset('assets/js/bootstrap.min.js') }}"></script>
+    <!-- Google Map Active js  -->
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBn-q0VMtMOum5A7HVG86duHeJApbVDv7o"></script>
+    <script src="{{ asset('assets/js/map.js') }}"></script>
+    <!-- contact-form Js -->
+    <script src="{{ asset('assets/js/contact-form.js') }}"></script>
+    <!-- Plugins Js -->
+    <script src="{{ asset('assets/js/plugins.js') }}"></script>
+    <!-- wow.min Js -->
+    <script src="{{ asset('assets/js/wow.min.js') }}"></script>
+    <!-- main Js -->
+    <script src="{{ asset('assets/js/main.js') }}"></script>
+
+</body>
+
+
+
+</html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -48,7 +48,7 @@
 
 <body>
 
-    
+
     @include('includes.header')
 
 
@@ -665,113 +665,8 @@
    <!--  Looking For Contsultant -->
 
 
-   <!-- Footer Top Area -->
-    <section id="footer-top" class="pt-100  pb-100">
-      <div class="container">
-        <!-- Start Row -->
-        <div class="row">
-          <!-- Start Widget -->
-            <div class="text-left col-lg-2 col-md-6 col-12 responsive-320">
-              <div class="footer-widget">
-                <div class="footer-logo">
-                  <img src="/assets/images/normal/melville.png" alt="Footer Logo">
-                </div>
-                <div class="widget-info">
-                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi eget fringilla erat. Morbi tempor diam.</p>
-                  <ul>
-                    <li><a href="#"><span><i class="icofont-facebook"></i></span></a></li>
-                    <li><a href="#"><span><i class="icofont-twitter"></i></span></a></li>
-                    <li><a href="#"><span><i class="icofont-pinterest"></i></span></a></li>
-                    <li><a href="#"><span><i class="icofont-vimeo"></i></span></a></li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-            <!-- End Widget -->
-          <!-- Start Widget -->
-            <div class="text-left  col-lg-3 col-md-6 col-12 responsive-320">
-              <div class="footer-widget">
-                <h3>Services</h3>
-                <ul>
-                  <li><a href="#">- After School Lessons</a></li>
-                  <li><a href="#">- Boarding School</a></li>
 
-                </ul>
-              </div>
-            </div>
-            <!-- End Widget -->
-          <!-- Start Widget -->
-            <div class="text-left responsive-320 col-lg-3 col-md-6 col-12">
-              <div class="footer-widget">
-                <h3>helpline</h3>
-                <ul>
-                  <li><a href="#">- Terms & Condition</a></li>
-                  <li><a href="#">- Helpline</a></li>
-                  <li><a href="#">- Privacy Policy</a></li>
-                  <li><a href="#">- Contact Us</a></li>
-                </ul>
-              </div>
-            </div>
-            <!-- End Widget -->
-          <!-- Start Widget -->
-            <div class="text-left responsive-320 col-lg-3 col-md-6 col-12">
-              <div class="footer-widget">
-                <h3>subscribe</h3>
-                <div class="widget-info">
-                  <p>Subscribe and be up to date with our school news letter </p>
-                  <div class="subscribe-here">
-                    <input type="email" placeholder="Your Email..." class="form-control" >
-                    <span><i class="icofont icofont-envelope"></i></span>
-                  </div>
-
-                </div>
-              </div>
-            </div>
-            <!-- End Widget -->
-
-
-        </div>
-        <!-- End Row -->
-      </div>
-    </section>
-   <!-- Footer Top Area -->
-
-   <!--  Footer Bottom Area -->
-    <footer id="footer-bottom">
-      <div class="container">
-        <!-- Start Row -->
-        <div class="row">
-          <!-- Start Col -->
-          <div class=" col-lg-6 col-md-12">
-            <div class="copy-text text-left">
-              <p>Copyrights © 2018 All Rights Reserved by <a href="#">Melville Group Of Schools</a></p>
-            </div>
-          </div>
-          <!-- End Col -->
-          <!-- Start Col -->
-          <div class="pt-25 col-lg-6 col-md-12">
-            <div class="footer-menu float-right">
-              <ul>
-                <li><a href="#">About Us</a></li>
-                <li><a href="#"> FAQ’s </a></li>
-
-              </ul>
-            </div>
-          </div>
-          <!-- End Col -->
-        </div>
-        <!-- End Row -->
-      </div>
-    </footer>
-   <!--  Footer Bottom Area -->
-
-    <!-- Back to scroll  -->
-    <div class="top-arrow">
-      <span class="scroll-top-top">
-        <i class="icofont-long-arrow-up" aria-hidden="true"></i>
-      </span>
-    </div>
-    <!-- Back to scroll  -->
+   @include('includes.footer')
 
     <!--All Jquery Library Files -->
     <script src="{{  asset('assets/js/vendor/jquery-3.3.1.min.js') }}"></script>
